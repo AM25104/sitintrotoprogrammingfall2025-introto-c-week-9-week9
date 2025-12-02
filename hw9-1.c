f442c980e398dbc3342e20747d4fcd7d80530f6e
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2025-12-02 23:12:54 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,18 @@
+// hw9-1.c
+#include <stdio.h>
+
+int main(void) {
+    double a, b, c, d;
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    scanf("%lf", &c);
+    scanf("%lf", &d);
+
+    double det = a * d - b * c;
+
+    printf("Determinant is %f\n", det);
+
+    return 0;
+}
+
 
