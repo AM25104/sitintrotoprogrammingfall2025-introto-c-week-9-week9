59e5fe8accdc7ef2ff5474d19fd4afb4b3a6d0b2
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2025-12-02 23:16:18 +0900

    Implement bubble sort for an array of integers

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,32 @@
 
+#include <stdio.h>
+
+int main(void) {
+    int a[5];
+    int i, j;
+
+   
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+    }
+
+   
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                int tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
+        }
+    }
+
+    printf("sorted array:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", a[i]);
+    }
+ 
+    return 0;
+}
+
+
