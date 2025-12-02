42d3e3b0eeeab660a6669c989f4aac4fce0f57b1
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2025-12-02 22:27:20 +0900

    Implement matrix multiplication in hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+    int A[3][2], B[2][3], C[3][3];
+
+    for(int i=0;i<3;i++)
+        for(int j=0;j<2;j++)
+            scanf("%d",&A[i][j]);
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<3;j++)
+            scanf("%d",&B[i][j]);
+
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++){
+            C[i][j] = 0;
+            for(int k=0;k<2;k++)
+                C[i][j] += A[i][k] * B[k][j];
+        }
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+            printf("%d ", C[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
+
 
