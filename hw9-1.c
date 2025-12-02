d673592fe5d777c0cec67b9153b11d92b48d0ad5
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2025-12-02 22:24:54 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    float a[2][2];
+    scanf("%f %f %f %f", &a[0][0], &a[0][1], &a[1][0], &a[1][1]);
+
+    printf("Matrix:\n");
+    printf("%.2f %.2f\n", a[0][0], a[0][1]);
+    printf("%.2f %.2f\n", a[1][0], a[1][1]);
+
+    float det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+    printf("Determinant = %.2f\n", det);
+    return 0;
+}
+
 
