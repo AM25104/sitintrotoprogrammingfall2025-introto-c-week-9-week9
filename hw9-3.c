8f402d08a913543add5ddce222c44b1a5e792d61
AM25104-NGUYEN-CANH-VAN-HA <am25104@shibaura-it.ac.jp>
2026-01-30 12:15:57 +0900

    hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -5,21 +5,21 @@ int main(void) {
     double B[3][2];
     double C[2][2];
 
-    /* Read first matrix A (2x3) */
+   
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 3; j++) {
             if (scanf("%lf", &A[i][j]) != 1) return 0;
         }
     }
 
-    /* Read second matrix B (3x2) */
+  
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 2; j++) {
             if (scanf("%lf", &B[i][j]) != 1) return 0;
         }
     }
 
-    /* Echo matrices (using %g to match formatting in examples) */
+  
     printf("The first matrix you entered is\n");
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 3; j++) {
@@ -36,7 +36,7 @@ int main(void) {
         printf("\n");
     }
 
-    /* Multiply: C = A (2x3) * B (3x2) => C (2x2) */
+   
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
             C[i][j] = 0.0;
@@ -56,5 +56,6 @@ int main(void) {
 
     return 0;
 }
+
 
 
